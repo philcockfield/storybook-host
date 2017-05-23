3feaaac82a85bb7ace7b17246d27eab8bfa76807
Robert Wawrzyniak <wawrob@de.ibm.com>
2017-05-23 14:20:19 +0200

    update storybook to 3.0.0-alpha

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import '../assets/css/normalize.css';
-import * as knobs from '@kadira/storybook-addon-knobs';
+import * as knobs from '@storybook/addon-knobs';
 
 export { knobs };
-export { Story, storiesOf, action } from '@kadira/storybook';
+export { Story, storiesOf, action } from '@storybook/react';
 export { R, Radium, css } from './common';
 export { host, host as default, IHostProps } from './decorators/host';
 export { AlignEdge } from './types';
