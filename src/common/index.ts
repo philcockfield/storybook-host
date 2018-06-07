2c4f7a2d3d6061ea81448f24d8a641e09a57c89b
Phil Cockfield <phil@cockfield.net>
2018-06-08 09:54:46 +1200

    Inline glamour `css` helpers from @tdb (with permission).

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,7 +1,6 @@
-import * as R from 'ramda';
-import * as Radium from 'radium';
-import css from './css';
+import * as css from './css';
 import * as color from './color';
 
+export { color, css };
 export * from './util';
-export { R, color, css, Radium };
+export * from './libs';
---
+++ src/common/libs.ts
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as R from 'ramda';
+import * as Radium from 'radium';
+
+export { R, Radium, React };
